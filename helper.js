a5a79a7b3f376e65667b28739a59942a4ee1f556
Sam Adrian <samadriansabalo99@gmail.com>
2022-12-02 22:34:59 +0800

    Added sound.preload = "auto"

--- helper.js
+++ helper.js
@@ -1,44 +1,20 @@
-export const togglePopper = function(popperInstance, button) {
-
-    function show() {
-        tooltip.setAttribute('data-show', '');
-
-        // We need to tell Popper to update the tooltip position
-        // after we show the tooltip, otherwise it will be incorrect
-        popperInstance.update();
-    }
-
-    function hide() {
-        tooltip.removeAttribute('data-show');
-    }
-
-    const showEvents = ['mouseenter', 'focus'];
-    const hideEvents = ['mouseleave', 'blur'];
-
-    showEvents.forEach((event) => {
-        button.addEventListener(event, show);
-    });
-
-    hideEvents.forEach((event) => {
-        button.addEventListener(event, hide);
-    });
-}
-
 export function makeToastNotification(message) {
-    let toastBox = document.querySelector(".toast-message")
+    let toastBox = document.getElementById("toast-message")
     toastBox.textContent = message
     toastBox.classList.add("active")
     setTimeout(() => {
         toastBox.classList.remove("active")
-    }, 3000);
+    }, 2000);
 }
 
+let sound = new Audio(`sounds/wrong.mp3`)
+sound.preload = "auto"
+
 export function makeError(message) {
-    let sound = new Audio(`sounds/wrong.mp3`)
     sound.play()
-    document.querySelector(".error-message").textContent = message
-    document.querySelector(".error").classList.add("active")
+    document.getElementById("error-message").textContent = message
+    document.getElementById("error").classList.add("active")
     setTimeout(() => {
-        document.querySelector(".error").classList.remove("active")
+        document.getElementById("error").classList.remove("active")
     }, 800);
 }
